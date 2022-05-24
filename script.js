564f9a36ce3e9663ace4dad2e1ccded14d52c832
Max <maxim.it@bk.ru>
2022-05-24 03:56:58 +0300

    speechSynthesis

---
+++ script.js
@@ -0,0 +1,21 @@
+// захватим UI-элементы для работы с ними
+const textEl = document.getElementById('text');
+const speakEl = document.getElementById('speak');
+
+// get voice
+let voice = null;
+window.speechSynthesis.onvoiceschanged = () => {
+    voice = window.speechSynthesis.getVoices()
+    console.log(voice)
+}
+
+// перехватим клик по кнопке
+speakEl.addEventListener('click', () => {
+    window.speechSynthesis.cancel()
+    const utterance = new SpeechSynthesisUtterance()
+    utterance.voice = voice[4]
+    utterance.rate = 2.5
+    utterance.text = textEl.value
+    utterance.pitch = 0
+    window.speechSynthesis.speak(utterance)
+});
