4105253a6e289da10b328246ba30d3dfb4c33323
Max <maxim.it@bk.ru>
2022-06-09 20:03:45 +0300

    irog

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ speakEl.addEventListener('click', () => {
     window.speechSynthesis.cancel()
     const utterance = new SpeechSynthesisUtterance()
     utterance.voice = voice[4]
-    utterance.rate = 2.5
+    utterance.rate = 1
     utterance.text = textEl.value
     utterance.pitch = 0
     window.speechSynthesis.speak(utterance)
